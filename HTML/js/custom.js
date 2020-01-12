54407d91d2359aafc23c622fbb585134c8471673
Danilo Soares <soaresoliva@gmail.com>
2020-01-12 01:05:53 -0300

    HTML - ultimos ajustes

--- HTML/js/custom.js
+++ HTML/js/custom.js
@@ -5,6 +5,7 @@ var home = {
 		this.carousel();
 		this.mobileMenu();
 		this.menuOnipage();
+		this.formulario();
 	},
 
 	animacao: function(){ 
@@ -32,18 +33,15 @@ var home = {
 
 	mobileMenu: function(){ 
 		$(".close-menu a").click(function(){ 
-
-			$(".main-menu").hide();
+			$(".main-menu").removeClass('show');
 			return false;
 		});
 	},
 
 	menuOnipage: function(){ 
-
 		$('.nav-main').onePageNav({
 	      currentClass: 'current-menu-item'
 	    });
-
 	    $(".nav-main a").click(function(){ 
 	    	var $anchor = $(this);
 
@@ -51,13 +49,58 @@ var home = {
 	            scrollTop: $($anchor.attr('href')).offset().top
 	        }, 1500);
 
+	    	$(".main-menu").removeClass('show');
 	        return false;
 	    });
+	},
 
-	   
+	formulario: function(){ 
+		$.mask.definitions['~'] = "[+-]";
+		$(".date-mask").mask("99/99/9999", {placeholder:"dd/mm/yyyy"});
+		$(".fone-mask").mask("(99) 9999-9999?");
+		$(".cep-mask").mask("99999-999", { completed: function() {  home.getCep(); } });
+	},
+
+	getCep: function(){
+
+		var _cep = $(".cep-mask").val().replace("-", "");
+
+		$.ajax({
+		  url: 'https://viacep.com.br/ws/' + _cep + '/json/',
+		  dataType: 'jsonp',
+		  crossDomain: true,
+		  contentType: "application/json",
+		  
+		}).done(function(data){
+
+			if(data.logradouro.length > 0)
+			{
+				$("input[name=Endereco]").val(data.logradouro);
+			}
+
+			if(data.bairro.length > 0)
+			{
+				$("input[name=Bairro]").val(data.bairro);
+			}
+
+			if(data.localidade.length > 0)
+			{
+				$("input[name=Cidade]").val(data.localidade);
+			}
+
+			if(data.uf.length > 0)
+			{
+				$("input[name=Estado]").val(data.uf);
+			}
+
+	    }).fail(function(jqXHR, textStatus, msg){
+	       alert("cep não encontrado");
+	    }); 
 	}
 }
 
 $(document).ready(function(){ 
 	home.init();
 });
+
+
